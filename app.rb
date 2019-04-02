f976ab9aba90dc051f3d447bff434e9bcd6c3e8c
flatiron <flatiron@BERLIN.local>
2019-04-02 13:00:49 +0100

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,12 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    @count_of_words = @analyzed_text.count_of_words
+    @count_of_vowels = @analyzed_text.count_of_vowels
+    @count_of_consonants = @analyzed_text.count_of_consonants
+    @most_used_letter = @analyzed_text.most_used_letter.first.first.capitalize
+    @biggest = @analyzed_text.most_used_letter.values.first
 
     erb :results
   end
